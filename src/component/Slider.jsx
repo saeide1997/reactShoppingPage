59bf16d2713920888714f9fb77c581890e2fec2a
saeide1997 <saeide.tj@gmail.com>
2024-08-05 13:18:55 +0330

    slider

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -1,6 +1,9 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
+import Carousel from 'react-bootstrap/Carousel';
+// import ExampleCarouselImage from 'components/ExampleCarouselImage';
 import { useState } from "react";
 import styled from "styled-components";
+
 
 const Slider = () => {
 
@@ -13,35 +16,31 @@ const Slider = () => {
 
 
     return (
-        <div className="w-[97%] h-[100vh] flex relative truncate m-10">
-            <div className="cursor-pointer w-[50px] h-[50px] bg-purple-100 rounded-full flex justify-center items-center absolute top-0 bottom-0 m-auto left-5">
-                <ArrowLeftOutlined />
-            </div>
-            <div className="h-[100%] flex">
-                <div className="flex items-center w-[100vw] h-[100vh]">
-                    <div className="flex-1 h-[100%]">
-                        <img className='h-[80%]' src="https://assistanteplus.fr/wp-content/uploads/2022/04/chat-midjourney.webp" alt="" />
-                    </div>
-                    <div className="flex-1 p-20">
-                        <h1 className="text-[70px]">فروش تابستانه</h1>
-                        <p className="my-12 mx-0 text-[20px]">فرصت استثنایی برای خرید با تخفیف!</p>
-                        <button className="border-solid border-1 p-3 text-[20px] cursor-pointer bg-transparent">الان ثبت کنید...</button>
-                    </div>
-                </div>
-                <div className="flex items-center w-[100vw] h-[100vh]">
-                    <div className="flex-1 h-[100%]">
-                        <img className='h-[80%]' src="https://assistanteplus.fr/wp-content/uploads/2022/04/chat-midjourney.webp" alt="" />
-                    </div>
-                    <div className="flex-1 p-20">
-                        <h1 className="text-[70px]">فروش زمستانه</h1>
-                        <p className="my-12 mx-0 text-[20px]">فرصت استثنایی برای خرید با تخفیف!</p>
-                        <button className="border-solid border-1 p-3 text-[20px] cursor-pointer bg-transparent">الان ثبت کنید...</button>
-                    </div>
-                </div>
-            </div>
-            <div  className="cursor-pointer w-[50px] h-[50px] bg-purple-100 rounded-full flex justify-center items-center absolute top-0 bottom-0 m-auto right-5">
-                <ArrowRightOutlined />
-            </div>
+        <div className=" ">
+        <Carousel>
+            <Carousel.Item className="h-[100vh] w-[100%]">
+                <img
+                    className="w-100 h-100"
+                    src="../assets/images/cat1.png"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className="h-[100vh] w-[100%]">
+                <img
+                    className="w-100 h-100"
+                    src="../assets/images/cat2.png"
+                    alt="Second slide"
+                />
+                <Carousel.Caption>
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
         </div>
     );
 };
