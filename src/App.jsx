c077ea7499979f06b0551cacd460057b3531b37c
saeide1997 <saeide.tj@gmail.com>
2024-08-10 11:08:36 +0330

    add print

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import ProductPage from "./pages/ProductPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import CartPage from "./pages/CartPage";
+import PrintPage from "./pages/PrintPage";
 
 const App = () => {
     return (
@@ -24,6 +25,7 @@ const App = () => {
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/register" element={<RegisterPage />} />
                     <Route path="/cart" element={<CartPage />} />
+                    <Route path="/print" element={<PrintPage />} />
                     <Route path="*" element={<NotFound />} /> 
                  </Routes>
             </BrowserRouter>
